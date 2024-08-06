0e2b097edb3f5dd99673c43f873efac6df24b816
unknown <mehmetkonuc@gmail.com>
2024-08-06 12:35:39 +0300

    blogs

--- apps/blogs/models.py
+++ apps/blogs/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.contenttypes.fields import GenericRelation
+from apps.photos.models import PhotosModel
 
 # Create your models here.
 class ArticlesModel(models.Model):
@@ -8,4 +10,5 @@ class ArticlesModel(models.Model):
                             on_delete=models.CASCADE)
     title = models.CharField(max_length=155)
     content = RichTextUploadingField()
+    futured_image = GenericRelation(PhotosModel)
     create_at = models.DateTimeField(auto_now_add=True)
--- apps/blogs/views.py
+++ apps/blogs/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from apps.blogs.forms import ArticleAddForm
 from apps.blogs.models import ArticlesModel
+from apps.photos.models import PhotosModel
+from django.contrib.contenttypes.models import ContentType
+from apps.comments.forms import CommentForm
+from apps.comments.models import Comment
+
 # Create your views here.
 class ArticleAddView(View):
     def get(self, request):
@@ -17,6 +22,13 @@ class ArticleAddView(View):
             form_data = form.save(commit=False)
             form_data.user = request.user
             form_data.save()
+            futured_image = request.FILES.get('futured_image')
+            PhotosModel.objects.create(
+                user=request.user,
+                content_type=ContentType.objects.get_for_model(form_data),
+                object_id=form_data.pk,
+                photo=futured_image
+            )
         context = {
             'form': form,
         }
@@ -33,10 +45,50 @@ class ArticlesView(View):
         return render(request, 'blogs/index.html', context)
 
 class ArticlesDetailsView(View):
+    form_class = CommentForm
+    model_comments = Comment
+
     def get(self, request, article_id):
         article = ArticlesModel.objects.filter(pk=article_id).first()
+        content_type = ContentType.objects.get_for_model(article)
+        comments = self.model_comments.objects.filter(content_type=content_type, object_id=article.id).order_by('-created_at')
 
         context = {
-            'article':article
+            'article':article,
+            'comments' :comments
         }
         return render(request, 'blogs/article-details.html', context)
+
+    def post(self, request, article_id):
+        article = ArticlesModel.objects.filter(pk=article_id).first()
+
+        content_type = ContentType.objects.get_for_model(article)
+        form = self.form_class(request.POST)
+        
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.content_type = content_type
+            comment.object_id = article.id
+            comment.save()
+            
+            images = request.FILES.getlist('images')
+            for image in images:
+                self.model_photos.objects.create(
+                    user=request.user,
+                    content_type=ContentType.objects.get_for_model(comment),
+                    object_id=comment.pk,
+                    photo=image
+                )
+            return redirect('article_details', article_id=article.id)
+        
+        comments = self.model_comments.objects.filter(content_type=content_type, object_id=article.id).order_by('-created_at')
+        # user_liked_posts = self.model_likes.objects.filter(content_type=content_type, user=request.user).values_list('object_id', flat=True)
+        
+        context = {
+            'form': form,
+            'comments' : comments,
+            'article' : article,
+            # 'user_liked_posts':user_liked_posts
+        }
+        return render(request, "blogs/article-details.html", context)
